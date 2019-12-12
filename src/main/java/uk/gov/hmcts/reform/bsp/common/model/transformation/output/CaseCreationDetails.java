830c3bc1e41334dad049a26046fa880e8c20268c
John O'Neill <johnoneill546@gmail.com>
2019-12-12 14:22:28 +0000

    BSP-157: Remove event ID from caseCreationDetails

--- src/main/java/uk/gov/hmcts/reform/bsp/common/model/transformation/output/CaseCreationDetails.java
+++ src/main/java/uk/gov/hmcts/reform/bsp/common/model/transformation/output/CaseCreationDetails.java
@@ -13,19 +13,14 @@ public class CaseCreationDetails {
     @JsonProperty("case_type_id")
     private final String caseTypeId;
 
-    @JsonProperty("event_id")
-    private final String eventId;
-
     @JsonProperty("case_data")
     private final Map<String, Object> caseData;
 
     public CaseCreationDetails(
         String caseTypeId,
-        String eventId,
         Map<String, Object> caseData
     ) {
         this.caseTypeId = caseTypeId;
-        this.eventId = eventId;
         this.caseData = caseData;
     }
 }
