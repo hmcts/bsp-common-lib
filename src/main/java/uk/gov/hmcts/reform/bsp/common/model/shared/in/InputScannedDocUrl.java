d4b81a3ca7908d34949812b5061380e0cb7b30c3
J. Victor Ferreira <victor.ferreira@hmcts.net>
2020-04-20 14:37:51 +0100

    Updating ExceptionRecord to handle scanned documents.

---
+++ src/main/java/uk/gov/hmcts/reform/bsp/common/model/shared/in/InputScannedDocUrl.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.bsp.common.model.shared.in;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class InputScannedDocUrl {
+
+    public final String url;
+
+    public final String binaryUrl;
+
+    public final String filename;
+
+    public InputScannedDocUrl(
+        @JsonProperty("document_url") String url,
+        @JsonProperty("document_binary_url") String binaryUrl,
+        @JsonProperty("document_filename") String filename
+    ) {
+        this.url = url;
+        this.binaryUrl = binaryUrl;
+        this.filename = filename;
+    }
+
+}
