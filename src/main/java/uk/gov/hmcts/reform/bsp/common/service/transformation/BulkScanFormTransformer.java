7790bdda178e75422e4b90cb41f9fb9f2556c2ad
John O'Neill <johnoneill546@gmail.com>
2020-06-16 15:22:33 +0100

    Removed redundant imports

--- src/main/java/uk/gov/hmcts/reform/bsp/common/service/transformation/BulkScanFormTransformer.java
+++ src/main/java/uk/gov/hmcts/reform/bsp/common/service/transformation/BulkScanFormTransformer.java
@@ -9,8 +9,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.bsp.common.config.BspCommonFields.BULK_SCAN_CASE_REFERENCE;
 
