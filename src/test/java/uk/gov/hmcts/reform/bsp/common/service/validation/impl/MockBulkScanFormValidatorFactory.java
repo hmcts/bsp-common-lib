913a22888be93e8fcb58ffb572b53ad79157b7ac
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-05-28 13:37:49 +0100

    Update java files to use jakarta

--- src/test/java/uk/gov/hmcts/reform/bsp/common/service/validation/impl/MockBulkScanFormValidatorFactory.java
+++ src/test/java/uk/gov/hmcts/reform/bsp/common/service/validation/impl/MockBulkScanFormValidatorFactory.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.bsp.common.service.validation.impl;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.bsp.common.service.validation.BulkScanFormValidatorFactory;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 
 @Component
